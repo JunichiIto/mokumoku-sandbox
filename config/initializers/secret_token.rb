4361c2f63a327ca17caa8370fc5de62666c99f51
Junichi Ito <jit@sonicgarden.jp>
2013-04-14 13:32:45 +0900

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MokumokuSandbox::Application.config.secret_token = '10694733f456bc079033472db08c7ad6b92a337052bbde3a8a63c241db7f862e092cc11fbdc37a9e2764f5f102e7ffec024ae32881be46cf868548e4e81bc882'
